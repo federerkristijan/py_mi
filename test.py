f96066e7ca99bab3638a2560d1143059e8fb4d79
Kristijan Federer <federer.kristijan@gmail.com>
2023-05-16 12:53:25 +0200

    test.py

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 # import math
 
 # result = math.e != math.pow(2, 4)
-# print(int(result)) # 1
+# print(int(result)) #
+
+# for ch in "abc":
+#     print(chr(ord(ch) + 1), end='')
+
+#bcd
+
+# for ch in "abc123XYX":
+#     if ch.isupper():
+#         print(ch.lower(), end='')
+#     elif ch.islower():
+#         print(ch.upper(), end='')
+#     else:
+#         print(ch, end='')
+
+# ABC123xyx
+
+
