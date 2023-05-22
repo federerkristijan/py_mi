1330f400930bf99c8a8158ea26af3b137d1b9a59
Kristijan Federer <federer.kristijan@gmail.com>
2023-05-22 09:29:48 +0200

    schüssell 3

--- blockchain/Tag_2/ceaser_verschluessung.py
+++ blockchain/Tag_2/ceaser_verschluessung.py
@@ -76,15 +76,51 @@ print("Nachricht:" ,nachricht) # QZUIPOJD
 # per Schleife
 
 # 1.
-key = 1
-message = "Wir Kinder vom Bahnhof Zoo"
-secret = ""
+# key = 1
+# message = "Wir Kinder vom Bahnhof Zoo"
+# secret = ""
 
-for char in secret:
+# for char in secret:
 
-    message.slice()
+#     message.slice()
 
-    if not char.isalpha():
+#     if not char.isalpha():
+#         continue
+
+#     code = ord(char) +- key
+
+nachricht = "Zoo" # Klartext
+schluessel = 3 # Schlüssel für die Verschiebung um 3 Stellen
+geheime_nachricht = "" # Platzhalter für die geheime Nachricht
+
+for char in nachricht:
+    if not char.isalpha(): # Kein Buchstabe vom Alphabet
         continue
 
-    code = ord(char) +- key
+    char = char.upper()
+    code = ord(char) + schluessel # Zeichencode des neuen Buchstabens nach der Verschiebung
+
+    if code > ord('Z'):
+        code = ord('A') + (code - ord('Z')) - 1
+
+    geheime_nachricht += chr(code)
+
+print("Geheime Nachricht:", geheime_nachricht) # CRR
+
+geheime_nachricht = "CRR" # Geheime Nachricht
+schluessel = 3 # Schlüssel für die Rückverschiebung um 3 Stellen
+nachricht = "" # Platzhalter für die entschlüsselte Nachricht
+
+for char in geheime_nachricht:
+    if not char.isalpha(): # Kein Buchstabe vom Alphabet
+        continue
+
+    char = char.upper()
+    code = ord(char) - schluessel # Zeichencode des ursprünglichen Buchstabens nach der Rückverschiebung
+
+    if code < ord('A'):
+        code = ord('Z') - (ord('A') - code) + 1
+
+    nachricht += chr(code)
+
+print("Entschlüsselte Nachricht:", nachricht) # ZOO
