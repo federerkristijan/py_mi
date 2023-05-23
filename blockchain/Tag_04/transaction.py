88b20cad8394a623ab9b79137d2d92ee212772d8
Kristijan Federer <federer.kristijan@gmail.com>
2023-05-23 10:41:42 +0200

    node.py

---
+++ blockchain/Tag_04/transaction.py
@@ -0,0 +1,8 @@
+import time
+
+class Transaction:
+    def __init__(self, sender, receiver, amount):
+        self.sender = sender,
+        self.receiver = receiver,
+        self.amount = amount,
+        self.time = time.time()
