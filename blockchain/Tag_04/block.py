b72eda5c9bf26f45d773ca6fbacbd5ce03e0431d
Kristijan Federer <federer.kristijan@gmail.com>
2023-05-23 15:09:33 +0200

    block.py - added nonce and hash.calculate_hash().  need implement this in the app.py

--- blockchain/Tag_04/block.py
+++ blockchain/Tag_04/block.py
@@ -1,4 +1,7 @@
 import time
+from hashlib import sha256
+
+difficulty = 3
 
 class Block:
 
@@ -8,8 +11,25 @@ class Block:
         self.transactions = transactions
         self.previous_hash = previous_hash
 
-        self.hash = hash
+        self.hash = hash.calculate_hash()
 
         self.time = time.time()
 
         self.nonce = 0 # vorerst OHNE proof-of-work, also Nonce aktuell IMMER bei 0 in unserer Demo
+
+    def calculate_hash(self):
+        computed_hash = ""
+
+        while not computed_hash.startswith("0" * difficulty):
+            self.nonce += 1
+            string_to_hash = "{0} {1} {2} {3} {4}".format(
+                self.index,
+                self.transactions,
+                self.time,
+                self.previous_hash,
+                self.nonce
+            )
+
+            computed_hash = sha256(string_to_hash.encode("utf-8")).hexdigest()
+
+        return computed_hash
