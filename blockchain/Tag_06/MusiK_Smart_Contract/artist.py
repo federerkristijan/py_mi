c7deb7cd11571065c4492cb4617a18f80ccba664
Kristijan Federer <federer.kristijan@gmail.com>
2023-05-25 13:04:58 +0200

    fixed imports

--- blockchain/Tag_06/MusiK_Smart_Contract/artist.py
+++
@@ -1,7 +0,0 @@
-class Artist():
-
-    def __init__(self, id, account, tracks):
-
-       self.id = id,
-       self.account = account,
-       self.tracks = tracks
--- blockchain/Tag_06/MusiK_Smart_Contract/user.py
+++
@@ -1,6 +0,0 @@
-class User():
-
-    def __init__(self, id, account):
-
-        self.id = id,
-        self.account = account
