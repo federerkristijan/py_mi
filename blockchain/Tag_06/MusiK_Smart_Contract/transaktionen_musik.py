01bf922499e655b48da8744ac2f678256167cf6c
Kristijan Federer <federer.kristijan@gmail.com>
2023-05-25 13:08:38 +0200

    transaction.buyer_id

--- blockchain/Tag_06/MusiK_Smart_Contract/transaktionen_musik.py
+++ blockchain/Tag_06/MusiK_Smart_Contract/transaktionen_musik.py
@@ -4,11 +4,12 @@
 
 class Transaction:
 
-    def __init__(self, artist_id, track_id, publish_date):
+    def __init__(self, artist_id, track_id, publish_date, buyer_id):
 
         self.artist_id = artist_id,
         self.track_id = track_id,
         self.publish_date = publish_date
+        self.buyer_id = buyer_id
 
         self.number_of_tracks = 0 # Default
         self.sold = False
