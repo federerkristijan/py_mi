3feb3d0746b89e3b0c077838e06982dc39b6c6b5
Kristijan Federer <federer.kristijan@gmail.com>
2023-05-25 08:59:59 +0200

    musik_smart_contract

---
+++ blockchain/Tag_06/Intro.py
@@ -0,0 +1,31 @@
+'''
+Guten Morgen zusammen,
+
+Ich melde mich ab 8:40 zu Wort um den Ablauf des praktischen Übungstags heute zu erläutern.
+
+Entweder:
+1) Übung von gestern als Vorlage nutzen
+Oder
+2) BC in Text-Adventure Grundgerüst integrieren (zB Handelstransaktionen von Gegenständen) - Link Gist folgt:
+https://gist.github.com/colonialars/99e63c656e92953870663182fe30f2db
+
+Oder
+3) ein kleines Monopoly-Spiel erstellen samt BC und (optional) smart contract, zB Immobilien erwerben, Miete zahlen - Link Wikipedia folgt:
+https://de.wikipedia.org/wiki/Monopoly#Das_Spielfeld
+
+Nice to have:
+- Versuchen einen Merkle-Tree oder eine Hashrate für die Nodes zu implementieren (zB Hashrate als Wahrscheinlichkeit zum Gewinn des PoW nutzen)
+
+Frohes Schaffen - die Übung von gestern und eure Projekte von heute dann gerne morgen früh mal präsentieren :) Bei Rückfragen/Problemen bin ich in Raum 1.
+
+Einige Anwendungsbeispiele BC/smart contracts:
+
+Es gibt viele praktische Anwendungsgebiete für Blockchains und den Einsatz von Smart Contracts. Hier sind einige Beispiele:
+ 1 Finanztransaktionen: Blockchains ermöglichen schnelle und sichere Transaktionen ohne Zwischenhändler wie Banken. Smart Contracts können automatisch Zahlungen auslösen, wenn bestimmte Bedingungen erfüllt sind.
+ 2 Lieferkettenmanagement: Blockchains können die Transparenz und Nachverfolgbarkeit von Lieferketten verbessern. Durch die Aufzeichnung von Informationen über Produkte und deren Herkunft können Verbraucher und Unternehmen sicherstellen, dass Produkte authentisch sind und aus ethisch vertretbaren Quellen stammen.
+ 3 Gesundheitswesen: Blockchains können die Interoperabilität von Gesundheitsdaten verbessern und den sicheren Austausch sensibler Informationen ermöglichen. Patientendaten können auf der Blockchain gespeichert und mit Einverständnis des Patienten für Forschungszwecke genutzt werden.
+ 4 Immobilien: Der Einsatz von Blockchains kann den Prozess des Kaufs, Verkaufs und der Übertragung von Immobilien beschleunigen und vereinfachen. Smart Contracts können den automatischen Transfer von Eigentumsrechten und Zahlungen ermöglichen, sobald bestimmte Bedingungen erfüllt sind.
+ 5 Versicherungen: Durch den Einsatz von Smart Contracts können Versicherungsansprüche automatisch abgewickelt werden, basierend auf vordefinierten Bedingungen. Dies erleichtert den Prozess der Schadensregulierung und reduziert die Kosten und den Zeitaufwand für alle beteiligten Parteien.
+ 6 Energiehandel: Blockchains können den Peer-to-Peer-Energiehandel ermöglichen, bei dem Energieproduzenten direkt mit Verbrauchern handeln können. Smart Contracts können den automatischen Austausch von Energie und Zahlungen basierend auf dem tatsächlichen Verbrauch ermöglichen.
+ 7 Urheberrechtsverwaltung: Blockchains können dazu beitragen, das Urheberrecht zu schützen, indem sie transparente und unveränderliche Aufzeichnungen von Urheberrechtsinformationen und Transaktionen bereitstellen. Künstler und Urheber können ihre Werke leichter schützen und die Rechte an ihnen verwalten.
+ '''
