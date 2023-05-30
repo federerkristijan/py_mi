ced0d0cf184ada09ee4dd7b7e59d5cf8d81e68eb
Kristijan Federer <federer.kristijan@gmail.com>
2023-05-30 11:47:11 +0200

    blockchain route

--- blockchain/Tag_07/app.py
+++ blockchain/Tag_07/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+from block import Block
+from transaction import Transaction
+
+from hashlib import sha256
 
 app = Flask(__name__)
 
@@ -34,22 +38,40 @@ def transaktion():
         print(sender, receiver, amount)
 
         # irgendwann mals das Ziel:
-        # transaktion1 = Transaktion(sender, receiver, amount)
+        transaktion1 = Transaction(sender, receiver, amount)
 
         # aktuelle Transaktion aus dem Formular in den Mempool übertragen
-        transaktionen.append("{0} schickt {1} {2} Coins").format(sender, receiver, amount) # vorerst NURS als String-Information
+        transaktionen.append(transaktion1)
 
         # print("POST Daten sind nicht da") # Ausgabe an den Client bzw. Browser
         # alle offenene Transaktion anzeigen
-        return transaktionen
+        return transaktion1.__dict__
 
     else:
         return render_template("index.html")
 
+# 1) Route /mine zum Minen eines Blocks (wichtig: Mempool leeren)
+@app.route("/mine")
+def mine(): # wichtig: hier Methoden erwähnen
+    global blockchain
+    global transaktionen
 
-@app.route("/mine", methods=["GET", "POST"])
-def mine():
-    pass
+    print("Mine")
+    print("Mempool:", transaktionen)
+
+    # ggfs. Previous Hash für Genesis-Block zuweisen
+    if len(blockchain) == 0:
+        previous_hash = "0000000000"
+    else:
+        previous_hash = blockchain[-1].hash
+
+    # Hash vereinfacht ohne PoW mit laufender Nummer per Anzahl der Elemente als EIngabe für sha256
+    hash = sha256(str(len(blockchain)).encode("utf-8")).hexdigest() # zB Hash für 0,1,2,3,...
+
+    blockchain.append(Block(len(blockchain), transaktionen, previous_hash, hash))
+
+    # Mempool leeren
+    transaktionen = []
 
 @app.route("/blockchain", methods=["GET", "POST"])
 def blockchain():
